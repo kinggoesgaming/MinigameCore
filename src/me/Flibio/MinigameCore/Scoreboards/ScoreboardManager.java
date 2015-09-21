c8b94cc0f8a527371d94367632e851c22276dccd
Flibio <Flibio@users.noreply.github.com>
2015-09-20 20:57:09 -0600

    Added scoreboard manager

---
+++ src/me/Flibio/MinigameCore/Scoreboards/ScoreboardManager.java
@@ -0,0 +1,60 @@
+package me.Flibio.MinigameCore.Scoreboards;
+
+import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class ScoreboardManager {
+	
+	public enum ScoreboardType {
+		LEADERBOARD
+	}
+	
+	private CopyOnWriteArrayList<CustomScoreboard> scoreboards = new CopyOnWriteArrayList<CustomScoreboard>();
+	
+	public ScoreboardManager() {
+		
+	}
+	
+	/**
+	 * Creates a new scoreboard
+	 * @param type
+	 * 	The type of scoreboard to create
+	 * @return
+	 * 	Optional of the created scoreboard
+	 */
+	public Optional<CustomScoreboard> createScoreboard(ScoreboardType type, String name) {
+		//TODO
+		return Optional.empty();
+	}
+	
+	/**
+	 * Gets a scoreboard
+	 * @param name
+	 * 	Name of the scoreboard to get
+	 * @return
+	 */
+	public Optional<CustomScoreboard> getScoreboard(String name) {
+		for(CustomScoreboard scoreboard : scoreboards) {
+			if(scoreboard.getName().toLowerCase().equalsIgnoreCase(name)) {
+				Optional.of(scoreboard);
+			}
+		}
+		return Optional.empty();
+	}
+	
+	/**
+	 * Checks if a scoreboard exists
+	 * @param name
+	 * 	Name of the scoreboard to check for
+	 * @return
+	 * 	Boolean based on if the scoreboard exists or not
+	 */
+	public boolean scoreboardExists(String name) {
+		for(CustomScoreboard scoreboard : scoreboards) {
+			if(scoreboard.getName().toLowerCase().equalsIgnoreCase(name)) {
+				return true;
+			}
+		}
+		return false;
+	}
+}

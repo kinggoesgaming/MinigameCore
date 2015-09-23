6ea548d12b28c96fbb67a59c356ef722a97c2a1d
Flibio <Flibio@users.noreply.github.com>
2015-09-23 16:34:18 -0600

    Implemented EconomyManager

--- src/me/Flibio/MinigameCore/Economy/EconomyManager.java
+++ src/me/Flibio/MinigameCore/Economy/EconomyManager.java
@@ -2,12 +2,24 @@ package me.Flibio.MinigameCore.Economy;
 
 import java.util.UUID;
 
+import me.Flibio.EconomyLite.API.EconomyLiteAPI;
+
 import org.spongepowered.api.Game;
+
+import com.google.common.base.Optional;
 
 public class EconomyManager {
 	
+	private boolean firstRun = true;
+	private boolean economyInstalled = false;
+	
+	private EconomyLiteAPI economyAPI;
+	
+	private Game game;
+	
+	//TODO - Support for TotalEconomy
 	public EconomyManager(Game game) {
-		
+		this.game = game;
 	}
 	
 	/**
@@ -18,8 +30,16 @@ public class EconomyManager {
 	 * 	Boolean based on if the method was successful or not
 	 */
 	public boolean setBalance(UUID uuid, double amount) {
-		//TODO
-		return false;
+		if(firstRun) {
+			checkForEconomy();
+		}
+		if(!economyInstalled) {
+			return false;
+		}
+		if(amount<0) {
+			return false;
+		}
+		return economyAPI.getPlayerAPI().setBalance(uuid.toString(), Integer.valueOf((int) Math.round(amount)));
 	}
 	
 	/**
@@ -29,9 +49,18 @@ public class EconomyManager {
 	 * @return
 	 * 	The balance of the player
 	 */
-	public double getBalance(UUID uuid) {
-		//TODO
-		return -1;
+	public Optional<Double> getBalance(UUID uuid) {
+		if(firstRun) {
+			checkForEconomy();
+		}
+		if(!economyInstalled) {
+			Optional.absent();
+		}
+		int bal = economyAPI.getPlayerAPI().getBalance(uuid.toString());
+		if(bal<0) {
+			return Optional.absent();
+		}
+		return Optional.of((double) bal);
 	}
 	
 	/**
@@ -44,8 +73,20 @@ public class EconomyManager {
 	 * 	Boolean based on if the method was successful or not
 	 */
 	public boolean addCurrency(UUID uuid, double amount) {
-		//TODO
-		return false;
+		if(firstRun) {
+			checkForEconomy();
+		}
+		if(!economyInstalled) {
+			return false;
+		}
+		if(amount<0) {
+			return false;
+		}
+		Optional<Double> currentBalance = getBalance(uuid);
+		if(!currentBalance.isPresent()) {
+			return false;
+		}
+		return setBalance(uuid, amount + currentBalance.get());
 	}
 	
 	/**
@@ -58,8 +99,31 @@ public class EconomyManager {
 	 * 	Boolean based on if the method was successful or not
 	 */
 	public boolean removeCurrency(UUID uuid, double amount) {
-		//TODO
-		return false;
+		if(firstRun) {
+			checkForEconomy();
+		}
+		if(!economyInstalled) {
+			return false;
+		}
+		if(amount<0) {
+			return false;
+		}
+		Optional<Double> currentBalance = getBalance(uuid);
+		if(!currentBalance.isPresent()) {
+			return false;
+		}
+		return setBalance(uuid, currentBalance.get() - amount);
+	}
+	
+	private void checkForEconomy() {
+		firstRun = false;
+		if(game.getPluginManager().getPlugin("EconomyLite").isPresent()) {
+			Optional<EconomyLiteAPI> service = game.getServiceManager().provide(EconomyLiteAPI.class);
+			if(service.isPresent()) {
+				economyInstalled = true;
+				economyAPI = service.get();
+			}
+		}
 	}
 	
 }

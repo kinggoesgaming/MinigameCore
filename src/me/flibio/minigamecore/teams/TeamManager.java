4087e08bf63032d96fbcb3c7ebba16ae3d62daa7
Flibio <flibio@users.noreply.github.com>
2015-10-12 16:43:48 -0600

    Added basic team management

--- src/me/flibio/minigamecore/teams/TeamManager.java
+++ src/me/flibio/minigamecore/teams/TeamManager.java
@@ -1,5 +1,78 @@
 package me.flibio.minigamecore.teams;
 
-public class TeamManager {
+import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 
+public class TeamManager {
+	
+	private CopyOnWriteArrayList<Team> teams = new CopyOnWriteArrayList<Team>();
+	
+	public TeamManager() {
+		
+	}
+	
+	/**
+	 * Gets a team from the list of teams
+	 * @param teamName
+	 * 	The name of the team to get
+	 * @return
+	 * 	The team
+	 */
+	public Optional<Team> getTeam(String teamName) {
+		for(Team team : teams) {
+			if(team.getName().equalsIgnoreCase(teamName)) {
+				return Optional.of(team);
+			}
+		}
+		return Optional.empty();
+	}
+	
+	/**
+	 * Checks if a team exists
+	 * @param teamName
+	 * 	The name of the team to check for
+	 * @return
+	 * 	If the team exists or not
+	 */
+	public boolean teamExists(String teamName) {
+		for(Team team : teams) {
+			if(team.getName().equalsIgnoreCase(teamName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Adds a team to the team list
+	 * @param team
+	 * 	The team to be added
+	 * @return
+	 * 	If the method was successful or not
+	 */
+	public boolean addTeam(Team team) {
+		if(teamExists(team.getName())) {
+			return false;
+		} else {
+			teams.add(team);
+			return true;
+		}
+	}
+	
+	/**
+	 * Removes a team from the list of teams
+	 * @param teamName
+	 * 	The name of the team to remove
+	 * @return
+	 * 	If the method was successful or not
+	 */
+	public boolean removeTeam(String teamName) {
+		for(Team team : teams) {
+			if(team.getName().equalsIgnoreCase(teamName)) {
+				teams.remove(team);
+				return true;
+			}
+		}
+		return false;
+	}
 }
